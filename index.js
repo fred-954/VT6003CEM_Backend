97145c4fdeb7392b9debe8e4db6a9a1fb2332604
fred954 <96521907+fred-954@users.noreply.github.com>
2022-05-25 05:35:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Koa = require('koa')
+const static = require('koa-static-router')
+const app = new Koa()
+
+const special = require('./routes/special')
+const articles = require('./routes/articles')
+const uploads= require('./routes/uploads')
+const users = require('./routes/users')
+const cors = require('@koa/cors');
+
+app.use(cors());
+app.use(special.routes())
+app.use(articles.routes())
+app.use(users.routes())
+app.use(uploads.routes())
+app.use(static({dir:'docs', router: '/doc/'}))
+
+let port = process.env.PORT || 10888;
+
+app.listen(port)
+console.log('API is ready', port)
