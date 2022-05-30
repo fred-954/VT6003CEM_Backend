1401f62b6e6f99d1596dd2fe08976ffa14f964c2
fred954 <96521907+fred-954@users.noreply.github.com>
2022-05-30 09:52:59 +0000

    Structure of table of table of dog is changed

--- sql/postgreSQL/dogs.sql
+++ sql/postgreSQL/dogs.sql
@@ -1,15 +1,11 @@
 CREATE TABLE public.dog (
 	id serial4 NOT NULL,
-	dogname varchar(32) NOT NULL,
-	dogType varchar(32) NULL,
-	dogAge varchar(32) NULL,
-	site varchar(32) NOT NULL,
-	CONSTRAINT users_pkey PRIMARY KEY (id)
+	dogname varchar(32) DEFAULT 'Unknown',
+	dogtype varchar(32) DEFAULT 'Unknown',
+	dogage varchar(32) DEFAULT 'Unknown',
+	site varchar(32) DEFAULT 'Unknown',
+  gender varchar(10) DEFAULT 'Unknown',
+  imgurl varchar(2048),
+  likes varchar(32),
+	CONSTRAINT dog_pkey PRIMARY KEY (id)
 );
-
-INSERT INTO dog (dogname,dogType,dogAge,site) VALUES
-	('John', 'Rabu','5','ST')
-INSERT INTO dog (dogname,dogType,dogAge,site) VALUES
-	('Candy', 'Rabu','2','TM')
-INSERT INTO dog (dogname,dogType,dogAge,site) VALUES
-	('Apple', 'Rabu','2','ST')
